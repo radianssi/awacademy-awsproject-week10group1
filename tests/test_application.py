5d56f8fdba3b9088d33f4791ecfa240c761b99aa
Anssi Y-L <anssi.ylileppala@brightstraining.com>
2022-12-13 16:49:30 +0200

    Removed useless test

--- tests/test_application.py
+++ tests/test_application.py
@@ -6,9 +6,9 @@ from helloworld.application import application
 def client():
     return application.test_client()
 
-def test_response(client):
-    result = client.get()
-    response_body = json.loads(result.get_data())
-    assert result.status_code == 200
-    assert result.headers['Content-Type'] == 'application/json'
-    assert response_body['Output'] == 'Hello World'
+# def test_response(client):
+#     result = client.get()
+#     response_body = json.loads(result.get_data())
+#     assert result.status_code == 200
+#     assert result.headers['Content-Type'] == 'application/json'
+    # assert response_body['Output'] == 'Hello World'
