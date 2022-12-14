8e900e7f2f33436714ea162cf89bf6a1b3e94768
Anssi Y-L <anssi.ylileppala@brightstraining.com>
2022-12-14 10:17:30 +0200

    Refactored to database version

--- src/application.py
+++ src/application.py
@@ -5,15 +5,27 @@ import optparse
 
 application = Flask(__name__)
 
-@application.route('/', methods=['GET'])
-# def get():
-#     return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
+# class Item(db.Model):
+#     id = db.Column(db.Integer(), primary_key=True)
+#     name = db.Column(db.String(length=30), nullable=False, unique=True)
+#     price = db.Column(db.Integer(), nullable=False)
+#     barcode = db.Column(db.String(length=12), nullable=False, unique=True)
+#     description = db.Column(db.String(length=1024), nullable=False, unique=True)
+
+#     def __repr__(self):
+#         return f'Item {self.name}'
+
+
+@application.route('/')
+@application.route('/home')
 def home_page():
     return render_template('home.html')
 
-@application.route('/', methods=['POST'])
-def post():
-    return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
+@application.route('/market')
+def market_page():
+    # items = Item.query.all()
+    return render_template('market.html')#, items=items)
+
 
 if __name__ == '__main__':
     default_port = "80"
